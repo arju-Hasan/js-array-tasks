2bdb957f8f17df3d876306a64fa02a52d8ab3e47
Arju-Hasan <rajuahmeed698@gmail.com>
2025-07-30 21:44:40 +0600

    add a another section

--- combining-arrays.js
+++ combining-arrays.js
@@ -12,3 +12,24 @@ console.log('Combined Fruits:', combinedFruits);
 // add a my choice array elements 
 combinedFruits.push('mango');
 console.log('After adding mango:', combinedFruits);
+
+
+
+
+
+
+
+// Create two arrays of your choice.
+let numbers1 = [1, 2, 3];
+let numbers2 = [4, 5, 6];
+// Use the concat method to combine the two arrays into a new array.
+
+let combinedNumbers = numbers1.concat(numbers2);
+// Print both the original arrays and the combined array using console.log().
+
+console.log('Numbers 1:', numbers1);
+console.log('Numbers 2:', numbers2);
+console.log('Combined Numbers:', combinedNumbers);
+// add a my choice array elements
+combinedNumbers.push(7);
+console.log('After adding 7:', combinedNumbers);
